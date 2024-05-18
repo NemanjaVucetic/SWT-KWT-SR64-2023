1f64950ccfcc30ad4d82c2c10afe712ebde991fc
Nemanja Vucetic <vucetic.nemanja2001@gmail.com>
2024-05-18 21:44:24 +0200

    greska

--- svt-2024-vezbe-main/svt1/src/main/java/rs/ac/uns/ftn/svtvezbe01/App.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.uns.ftn.svtvezbe01;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- svt-2024-vezbe-main/svt3/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO nastavnici
-VALUES (1, 'Petar', 'Petrovic', 'docent');
-
-INSERT INTO predmeti
-VALUES (1, 'Matematika');
